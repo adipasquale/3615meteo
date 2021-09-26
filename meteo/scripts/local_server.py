9617523ebcfb0b0775c6312a73b912882445e8de
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-09-26 18:01:16 +0200

    improve local_server script to support skip fetch option

--- meteo/scripts/local_server.py
+++ meteo/scripts/local_server.py
@@ -1,19 +1,26 @@
 from livereload import Server, shell
 import os
+import argparse
 
 DIRNAME = os.path.dirname(__file__)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--skip-fetch", help="uses pre-existing XMLs as cache", action="store_true")
+args = parser.parse_args()
+cmd = f'poetry run python -m meteo.scripts.build {"--skip-fetch" if args.skip_fetch else ""}'
 
 server = Server()
 server.watch(
   os.path.join(DIRNAME, '../templates/*.jinja'),
-  shell('poetry run python -m meteo.scripts.build')
+  shell(cmd)
 )
 server.watch(
   os.path.join(DIRNAME, '../templates/*.html'),
-  shell('poetry run python -m meteo.scripts.build')
+  shell(cmd)
 )
 server.watch(
   os.path.join(DIRNAME, '../**/*.py'),
-  shell('poetry run python -m meteo.scripts.build')
+  shell(cmd)
 )
 server.serve(root=os.path.join(DIRNAME, '../../docs'))
